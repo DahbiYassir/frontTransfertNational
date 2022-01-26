92a448a84dd6a3c9542c1161692159ed84a8d595
test <macbook@associationkasrelbadia@gmail.com>
2022-01-26 22:09:08 +0100

    working on dashboard

---
+++ src/app/agent.ts
@@ -0,0 +1,10 @@
+export interface Agent {
+    idAgent : number;
+    refAgent : String;
+    nom : String ;
+    prenom : String ;
+    cin : String ;
+    telephone : number ;
+    dateCreation : Date ;
+    plafond : number;
+}
--- src/app/dashboard/dashboard-components/top-cards/top-cards.component.ts
+++ src/app/dashboard/dashboard-components/top-cards/top-cards.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {topcard,topcards} from './top-cards-data';
+import {topcard} from './top-cards-data';
+import { TransfertNationalService } from '../../../services/transfert-national.service';
+import { TransfertNational } from '../../../transfert-national';
+import { HttpErrorResponse } from '@angular/common/http/http';
 
 @Component({
   selector: 'app-top-cards',
@@ -7,14 +10,67 @@ import {topcard,topcards} from './top-cards-data';
 })
 export class TopCardsComponent implements OnInit {
 
-  topcards:topcard[];
+  topcards:topcard[]=[];
+  x : string ="test string "
+  transferts : TransfertNational[] = [];
+  todayTf : TransfertNational[] = [];
+  nombreTf : number = 0 ;
+  nombreString : string ="";
 
-  constructor() { 
+  constructor(private transfertService : TransfertNationalService) { 
 
-    this.topcards=topcards;
+  
   }
 
   ngOnInit(): void {
+    this.getTransferts();
   }
 
+  getTransferts(){
+    this.transfertService.getTransferts().subscribe(
+      (response : any) =>{
+        this.transferts = response;
+        this.todaysss();
+        this.montant();
+      },
+      (error : HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    )
+  }
+
+  todaysss(){
+    this.transferts.forEach(e =>{
+      var today  = new Date();
+      let newDate = new Date(e.dateCreation);
+      if(newDate.getDay() == today.getDay()){
+        this.todayTf.push(e);
+      }
+    })
+    console.log("today's trasnfert");
+    console.log(this.todayTf);
+    this.nombreTf = this.todayTf.length;
+    this.nombreString = this.nombreTf.toString();
+    console.log(this.nombreString)
+
+    this.topcards=[{
+      bgcolor: 'success',
+      icon: 'bi bi-arrow-down-up',
+      title: this.nombreTf,
+      subtitle: 'Nombre de Transferts'
+    }];
+  }
+  total : number = 0;
+  montant(){
+    this.transferts.forEach(e=>{
+      this.total = this.total + e.montant;
+    })
+    console.log("total : " +this.total);
+    this.topcards.push({
+      bgcolor: 'danger',
+      icon: 'bi bi-cash',
+      title: this.total,
+      subtitle: 'MAD'
+    });
+  }
 }
