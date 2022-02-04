51336fd54a8fec7fd1237bc89491acc5b5e88d22
imane <imanesbai.sbai@gmail.com>
2022-02-05 00:57:00 +0100

    wiwi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl : 'https://client-servicee.herokuapp.com',
+  apiClientCompteUrl : 'https://client-servicee.herokuapp.com',
+  apiTransfertUrl : 'https://transfert-servicee.herokuapp.com',
+  apiAgentUrl : 'https://agent-servicee.herokuapp.com'
+
 };
